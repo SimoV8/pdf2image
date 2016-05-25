3eab9ec3b0f310fc3c878a7a0811cb66ccb596f9
Simone Vuotto <simov8@gmail.com>
2016-05-25 12:09:10 +0200

    support for multi page pdf files added

--- pdf2image.py
+++ pdf2image.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from PythonMagick import Image
-
+from pyPdf import PdfFileWriter, PdfFileReader
 
 def main():
     # os.environ["MAGICK_HOME"] = r"path_to_ImageMagick"
@@ -11,17 +11,34 @@ def main():
     else:
         dirpath = sys.argv[1]
 
-    for path in os.listdir(dirpath):
-        if not os.path.isfile(path) or not path.endswith('.pdf'):
-            continue
-        print('Converting file {} ...'.format(path))
-        file = os.path.join(dirpath, path)
-        to = file.replace(".pdf", ".png")
+    temp_dir = os.path.join(dirpath, '.temp')
+    if not os.path.exists(temp_dir):
+        os.mkdir(temp_dir)
 
-        p = Image()
-        p.density('1080')
-        p.read(os.path.abspath(file))
-        p.write(os.path.abspath(to))
+    for file_name in os.listdir(dirpath):
+        if not os.path.isfile(file_name) or not file_name.endswith('.pdf'):
+            continue
+        print('Converting file {} ...'.format(file_name))
+        in_path = os.path.join(dirpath, file_name)
+        with open(in_path, 'rb') as handle:
+            inputpdf = PdfFileReader(handle)
+            for i in xrange(inputpdf.numPages):
+                outputpdf = PdfFileWriter()
+                outputpdf.addPage(inputpdf.getPage(i))
+                new_file_name = file_name.replace('.pdf', '') + '_{}.pdf'.format(i)
+                new_in_path = os.path.join(temp_dir, new_file_name)
+                with open(new_in_path, 'wb') as handle:
+                    outputpdf.write(handle)
+
+                output_file_name = new_file_name.replace('.pdf', '.jpeg')
+                output_path = os.path.join(dirpath, output_file_name)
+                p = Image()
+                p.density('1080')
+                p.read(os.path.abspath(new_in_path))
+                p.write(os.path.abspath(output_path))
+                os.remove(new_in_path)
+        os.rmdir(temp_dir)
+
 
 if __name__ == '__main__':
     main()
